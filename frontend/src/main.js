0b33387cec15416a018c80dbc20942580f8ec176
gibeomKim <ejsjdj@naver.com>
2025-10-05 23:11:21 +0900

    feat(vue3): vue3 환경설정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,14 @@
-import './assets/main.css'
+import {createApp} from 'vue';
+import {createPinia} from 'pinia';
+import App from './App.vue';
+import router from './router';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+const app = createApp(App);
 
-import App from './App.vue'
-import router from './router'
+app.use(createPinia());
+app.use(router);
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+// 마운트 시점변경
+router.isReady().then(() => {
+    app.mount('#app');
+});
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+    <div class="container">
+        <!-- mt-5 는 margin-top 5 라는 뜻 -->
+        <h1 class="mt-5">HELLO, HOME</h1>
+    </div>
+</template>
